379da92dd3ffc638f13028e4b2f6f3a37f5e060d
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-08 18:05:55 +0300

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ function NavBar() {
   const links = ["home", "about", "projects"];
 
   return(
-  <nav>
-    <a href="#home">home</a>
-    <a href="#about">about</a>
-    <a href="#projects">projects</a>
+    <nav>
+    {links.map((link) => (
+      <a key={link} href={"#" + link}>{link}</a>
+    ))}
   </nav>
   );
 }
